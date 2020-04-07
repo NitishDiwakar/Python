83c627e02ffcd288c25d704cd697b0fa3dec3924
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2020-04-07 23:44:03 +0530

    Addition of two numbers provied by user

---
+++ add.py
@@ -0,0 +1,11 @@
+# This program add two numbers provided by user:
+
+# Store input numbers:
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers:
+sum = float(num1 + num2)
+
+# Display the sum 
+print('The sum of {0} and {1} is {2} '. format(num1, num2, sum))
