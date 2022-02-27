18b32014accc16f29848261fc4f23dd37f657bd4
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2022-02-27 22:22:27 +0530

    Python program to multiplication two numbers

---
+++ multiplication.py
@@ -0,0 +1,11 @@
+# This program multiply two number provided by user
+
+# Get two numbers
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+# Multiply these two numbers
+num3 = float(num1 * num2)
+
+# Print result
+print('Multiplication of {0} and {1} is {2}' . format(num1, num2, num3))
