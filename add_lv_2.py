ad61d243be8950d02500bca6c9740f4da3a77be8
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2023-06-09 09:52:46 +0530

    Repeat 5 times

--- add_lv_2.py
+++ add_lv_2.py
@@ -8,3 +8,49 @@ print(rand_list)
 
 input('')
 print(sum(rand_list))
+print('')
+
+# Show digits 2nd time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Show ditigs 3rd time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Show digits 4th time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+
+# Show digits 5th time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
