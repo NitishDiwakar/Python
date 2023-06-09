6bec53c0dab77c09bbea673e2311bb4aadd1711a
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2023-06-09 09:55:28 +0530

    Updated for 5

--- add_lv_3.py
+++ add_lv_3.py
@@ -8,3 +8,48 @@ print(rand_list)
 
 input('')
 print(sum(rand_list))
+print('')
+
+# Repeat 2nd time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 3rd time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 4th time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 5th time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,39))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
