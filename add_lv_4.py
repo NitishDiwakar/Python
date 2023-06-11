13a1bc1a61601651017eb9b73bca34fe51c8ebee
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2023-06-11 16:16:41 +0530

    New level added

---
+++ add_lv_4.py
@@ -0,0 +1,56 @@
+import random
+ 
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Show digits 2nd time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Show ditigs 3rd time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Show digits 4th time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+
+# Show digits 5th time
+rand_list=[]
+n=4
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
---
+++ add_lv_5.py
@@ -0,0 +1,55 @@
+import random
+ 
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 2nd time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 3rd time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 4th time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
+
+# Repeat 5th time
+rand_list=[]
+n=10
+for i in range(n):
+    rand_list.append(random.randint(10,99))
+print(rand_list)
+
+input('')
+print(sum(rand_list))
+print('')
