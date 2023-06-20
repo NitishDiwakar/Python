412ff54c26c05b52dd6ff1629d15865c3663853c
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2023-06-20 15:01:37 +0530

    Square

---
+++ sq_11_to_20.py
@@ -0,0 +1,50 @@
+import random
+ 
+# rand_list=[]
+# n=1
+# for i in range(n):
+
+digit = random.randint(11,20)
+print(digit)
+
+input('')
+# print(sum(rand_list))
+print((digit * digit))
+print('')
+
+# Show digits 2nd time
+digit = random.randint(11,20)
+print(digit)
+
+input('')
+# print(sum(rand_list))
+print((digit * digit))
+print('')
+
+# Show digits 3rd time
+digit = random.randint(11,20)
+print(digit)
+
+input('')
+# print(sum(rand_list))
+print((digit * digit))
+print('')
+
+# Show digits 4th time
+digit = random.randint(11,20)
+print(digit)
+
+input('')
+# print(sum(rand_list))
+print((digit * digit))
+print('')
+
+
+# Show digits 5th time
+digit = random.randint(11,20)
+print(digit)
+
+input('')
+# print(sum(rand_list))
+print((digit * digit))
+print('')
